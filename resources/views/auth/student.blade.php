36fb257af4952ae4b0b8a2fa3d9d4a4c1f49726d
germanybasillo <160084953+germanybasillo@users.noreply.github.com>
2024-04-06 13:05:24 +0800

    Update students.blade.php

--- resources/views/auth/student.blade.php
+++ resources/views/auth/student.blade.php
@@ -98,8 +98,13 @@
                                 <p class="text-muted mb-0">Student</p>
                             </div>
                         </div>
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </div>
                 </li>
             </ul>
