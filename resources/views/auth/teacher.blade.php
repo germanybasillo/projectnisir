ee105f880e5882331df789b62c30b20fd2d0ff69
germanybasillo <160084953+germanybasillo@users.noreply.github.com>
2024-04-06 13:06:53 +0800

    Update teacher.blade.php

--- resources/views/auth/teacher.blade.php
+++ resources/views/auth/teacher.blade.php
@@ -98,7 +98,13 @@
                                 <p class="text-muted mb-0">Teacher</p>
                             </div>
                         </div>
-                        <a class="dropdown-item" href="login.html">Logout</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            Logout
+                        </a>
+                        
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </div>
                 </li>
             </ul>
